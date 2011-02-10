c5764e7493e84744b40312925f95ff9e99f8be68
Paul <brower.paul@gmail.com>
2011-02-09 23:14:10 -0500

    home and tools controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+  def index
+    Rails.logger.debug("home index called")
+
+  end
+
+  def login
+
+  end
+
+end
---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,13 @@
+class ToolsController < ApplicationController
+#  before_filter :login_required
+  # GET /tools
+  # GET /tools.xml
+  def index
+    
+  end
+
+  def who_to_follow
+    Rails.logger.debug("who to follow called")
+  end
+
+end
