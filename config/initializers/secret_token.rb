927b9c7e89d5c2047f72b8e4055a16991f0f72b2
Paul <brower.paul@gmail.com>
2011-02-09 21:44:08 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittertool2::Application.config.secret_token = '4d615f3d819e67593487438fa665012f0618ef724395d968bc5ad0c027e2349a8e7b9a431f102d68463384dbc36fd53b96a683bbb65b835b0b49ddcd6b3fcca5'
---
+++ vendor/plugins/twitter-auth/rails/init.rb
@@ -0,0 +1,8 @@
+# Gem Dependencies
+#config.gem 'oauth'
+#config.gem 'ezcrypto'
+#
+require 'json'
+require 'twitter_auth'
+
+Rails.logger.info("** TwitterAuth initialized properly.")
